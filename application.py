6666ebec4a63f74c9861be2d7932ffa0e9baf96a
samirrajpurkar <samir.rajpurkar@gmail.com>
2018-10-19 13:31:44 +0400

    registration complete

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -24,3 +24,38 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+  return render_template("register.html")
+
+@app.route("/books")
+def books():
+  books = db.execute("SELECT * FROM books").fetchall()
+  return render_template("books.html", books=books)
+
+@app.route("/register_user", methods=["POST"])
+def register_user():
+  print(request.form.get("username"))
+  print(request.form.get("password"))
+  print(request.form.get("rememberme"))
+
+  rememberme = True if request.form.get("rememberme") == "on" else False 
+
+  try:
+    username = request.form.get("username")
+  except ValueError:
+    return render_template("error_register_user.html", message="Invalid username.")
+
+  try:
+    password = request.form.get("password")
+  except ValueError:
+    return render_template("error_register_user.html", message="Invalid password")
+
+  # check if the same username exists in the database
+  # if so return error
+  if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).rowcount == 1:
+    return render_template("error_register_user.html", message="User already exists..sorry!")
+  db.execute("INSERT INTO users (username, password, rememberme) VALUES(:username, :password, :rememberme)", {"username": username, "password": password, "rememberme": rememberme})
+  db.commit()
+  return redirect(url_for('books'))
